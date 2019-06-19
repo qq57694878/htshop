f1065432500dc22a6f35bdde7d5aa606380b3df1
qq57694878 <57694878@qq.com>
2019-06-20 00:40:47 +0800

    登录注册

---
+++ mp-wechat/pages/login/register.js
@@ -0,0 +1,99 @@
+// pages/login/register.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    mobile:'',
+    password:'',
+    carNo:'',
+    isShow:false,
+    keyBoardType:"1",
+    repassword:"",
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  inputOk(){
+    this.setData({ isShow: false });
+  },
+  inputdelete(){
+    this.setData({ carNo: '' });
+  },
+  inputChange(e){
+ 
+    if(e.detail){
+      this.setData({ carNo: this.data.carNo + e.detail });
+    }
+    let keyBoardType = 1
+    if (this.data.carNo == '') {
+      keyBoardType = 1
+    } else {
+      keyBoardType = 2
+    }
+    this.setData({ keyBoardType: keyBoardType}); 
+  },
+  onCarnoFocus(){
+    let keyBoardType=1
+    if (this.data.carNo=='') {
+      keyBoardType = 1
+    } else {
+      keyBoardType = 2
+    }
+    this.setData({ isShow: true, keyBoardType: keyBoardType});
+  }
+})
